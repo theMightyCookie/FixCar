4b5a5966b4752a10be9ad4216ddc4b9cfa413698
cookie <andreea.amaria7@gmail.com>
2023-02-05 17:13:18 +0200

    add mechanicReviews

--- app/src/main/java/com/example/fixcar/mechanicReviews.java
+++ app/src/main/java/com/example/fixcar/mechanicReviews.java
@@ -1,8 +1,24 @@
 package com.example.fixcar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class mechanicReviews extends AppCompatActivity {
 
@@ -10,5 +26,34 @@ public class mechanicReviews extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mechanic_reviews);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://fixcar-1b5cf-default-rtdb.firebaseio.com");
+        FirebaseUser currentuser = FirebaseAuth.getInstance().getCurrentUser();
+
+        final EditText review = findViewById(R.id.reviewRead);
+
+        databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                assert currentuser != null;
+                if (!(snapshot.child(currentuser.getUid()).child("mechanic").child("review").getValue() == null))
+                {
+                    review.setText(Objects.requireNonNull(snapshot.child(currentuser.getUid()).child("mechanic").child("review").getValue()).toString());
+                }
+                else{
+                    review.setText("Nu exista recenzii");
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
     }
 }
