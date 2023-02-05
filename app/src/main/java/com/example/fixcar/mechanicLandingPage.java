f73ad12d1d26ea427ef6294a77a267e3b3bcf189
cookie <andreea.amaria7@gmail.com>
2023-02-05 16:09:50 +0200

    add mechanicViewClients

--- app/src/main/java/com/example/fixcar/mechanicLandingPage.java
+++ app/src/main/java/com/example/fixcar/mechanicLandingPage.java
@@ -1,11 +1,23 @@
 package com.example.fixcar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckedTextView;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class mechanicLandingPage extends AppCompatActivity {
 
@@ -14,12 +26,36 @@ public class mechanicLandingPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mechanic_landing_page);
 
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://fixcar-1b5cf-default-rtdb.firebaseio.com");
+        FirebaseUser currentuser = FirebaseAuth.getInstance().getCurrentUser();
+
         final Button settingsBtn = findViewById(R.id.config);
         final Button reviewsBtn = findViewById(R.id.btnReviews);
         final Button homeBtn = findViewById(R.id.btnHome);
         final Button profileBtn = findViewById(R.id.btnProfile);
         final Button scheduleBtn = findViewById(R.id.btnOrar);
         final Button clientProfileBtn = findViewById(R.id.btnClientProfile);
+        final CheckedTextView appointments = findViewById(R.id.programari);
+
+        databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                assert currentuser != null;
+                if (!(snapshot.child(currentuser.getUid()).child("mechanic").child("appointment").getValue() == null))
+                {
+                    appointments.setText(snapshot.child(currentuser.getUid()).child("mechanic").child("appointment").getValue().toString());
+                }
+                else{
+                    appointments.setText("Nu exista programari!");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/fixcar/mechanicViewClients.java
+++ app/src/main/java/com/example/fixcar/mechanicViewClients.java
@@ -1,14 +1,118 @@
 package com.example.fixcar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class mechanicViewClients extends AppCompatActivity {
+
+    EditText searchText;
+    ListView listView;
+    AutoCompleteTextView textView;
+    DatabaseReference databaseReference;
+    DatabaseReference dbRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mechanic_view_clients);
+        setContentView(R.layout.activity_client_search_bar);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://fixcar-1b5cf-default-rtdb.firebaseio.com");
+        dbRef = databaseReference.child("users");
+
+        listView = (ListView) findViewById(R.id.listData);
+        textView = (AutoCompleteTextView) findViewById(R.id.textSearch);
+
+        ValueEventListener event = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                populateSearch(snapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        databaseReference.child("users").addListenerForSingleValueEvent(event);
+
+
     }
+
+    private void populateSearch(DataSnapshot snapshot){
+        Log.d("users", "reading data");
+        ArrayList<String> names = new ArrayList<>();
+        if(snapshot.exists())
+        {
+            for (DataSnapshot ds:snapshot.getChildren())
+            {
+                String name = ds.child("username").getValue(String.class);
+                names.add(name);
+            }
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names);
+            textView.setAdapter(adapter);
+            textView.setOnItemClickListener((parent, view, position, id) -> {
+                String name = textView.getText().toString();
+                System.out.print(name);
+                searchUser(name);
+            });
+
+        }
+        else
+        {
+            Log.d("users", "No data found");
+            Toast.makeText(mechanicViewClients.this, "Nu au fost identificate date!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void searchUser(String name) {
+        Query query = dbRef.orderByChild(name).equalTo(name);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if(snapshot.exists()){
+                    ArrayList<String> listusers = new ArrayList<>();
+                    for(DataSnapshot ds:snapshot.getChildren()){
+                        // User user = new User(ds.child("username").getValue(String.class), ds.child("email").getValue(String.class));
+                        listusers.add(ds.child("username").getValue(String.class)+"\n"+ds.child("email").getValue(String.class));
+                        System.out.print(listusers.toString());
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, listusers);
+                    listView.setAdapter(adapter);
+
+                }
+                else
+                {
+                    Log.d("users", "No details found");
+                    Toast.makeText(mechanicViewClients.this, "Nu au fost identificate detalii!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
 }
